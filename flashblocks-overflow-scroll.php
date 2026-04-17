5b83ce601bc21b86026543f6cd091b1844ab14b0
sun <sunny@sunnymorgan.com>
2026-04-17 13:47:40 -0400

    * Flashblocks Module: yes

--- flashblocks-overflow-scroll.php
+++ flashblocks-overflow-scroll.php
@@ -9,6 +9,9 @@
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       flashblocks
+ * Flashblocks Module: yes
+ * Flashblocks Category: layout
+ * Flashblocks Tags: overflow, scroll, container
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
